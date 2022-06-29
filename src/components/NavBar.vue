76d7579cf55a40647debcaa337acf9703bb3e460
AlfClark <alfredoclarkcardenas@gmail.com>
2022-06-29 10:07:57 -0700

    Navbar elements, initial styling

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,50 @@
 <template>
-  <nav></nav>
+  <nav class="navbar">
+    <div class="burger-menu"><i class="fa-solid fa-bars"></i></div>
+    <div class="brand">
+      <router-link to="/">
+        <img class="logo" src="@/assets/logo.png" alt="" />
+      </router-link>
+    </div>
+    <div class="tools">
+      <router-link to=""><i class="fa-solid fa-cart-shopping"></i></router-link>
+      <router-link to=""><i class="fa-solid fa-user"></i></router-link>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+.navbar {
+  padding: 0;
+  display: flex;
+  width: 100vw;
+  align-items: center;
+  justify-content: space-between;
+  margin: 1rem 0;
+  padding: 0 3rem;
+}
+
+.logo {
+  width: 100px;
+  margin: 0;
+}
+
+.tools {
+  display: flex;
+}
+
+.fa-solid {
+  color: var(--carbon);
+  font-size: 1.5rem;
+}
+
+@media screen and (max-width: 900px) {
+  .logo {
+    width: 60px;
+  }
+}
+</style>
