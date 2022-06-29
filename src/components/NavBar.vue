fa7f9b564d95f2720bc5709d954e711cdf6912d2
AlfClark <alfredoclarkcardenas@gmail.com>
2022-06-29 11:30:03 -0700

    Navbar design/style done

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,8 @@
 <template>
   <nav class="navbar">
-    <div class="burger-menu"><i class="fa-solid fa-bars"></i></div>
+    <div class="burger-menu">
+      <i onclick="showMenu()" class="fa-solid fa-bars"></i>
+    </div>
     <div class="brand">
       <router-link to="/">
         <img class="logo" src="@/assets/logo.png" alt="" />
@@ -8,13 +10,19 @@
     </div>
     <div class="tools">
       <router-link to=""><i class="fa-solid fa-cart-shopping"></i></router-link>
-      <router-link to=""><i class="fa-solid fa-user"></i></router-link>
     </div>
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  setup() {
+    const showMenu = () => {};
+    return {
+      showMenu,
+    };
+  },
+};
 </script>
 
 <style>
@@ -26,11 +34,22 @@ export default {};
   justify-content: space-between;
   margin: 1rem 0;
   padding: 0 3rem;
+  top: 0;
+}
+
+.burger-menu {
+  cursor: pointer;
 }
 
 .logo {
   width: 100px;
   margin: 0;
+  transition: 1s;
+}
+
+.logo:hover {
+  transform: scale(1.05);
+  transition: 0.4s;
 }
 
 .tools {
@@ -40,11 +59,20 @@ export default {};
 .fa-solid {
   color: var(--carbon);
   font-size: 1.5rem;
+  transition: 0.6s;
+}
+
+.fa-solid:hover {
+  color: grey;
+  transition: 0.4s;
 }
 
 @media screen and (max-width: 900px) {
   .logo {
     width: 60px;
+  }
+  .fa-solid {
+    font-size: 1rem;
   }
 }
 </style>
