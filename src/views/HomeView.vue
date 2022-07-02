0f46e2ebc7d40e13c4a053191c04d97dfb2dd983
AlfClark <alfredoclarkcardenas@gmail.com>
2022-07-01 19:57:17 -0700

    Products overview landpage component call

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,16 @@
 <template>
   <LandingScreen />
+  <ProductsOverview />
 </template>
 
 <script>
 import LandingScreen from "@/components/onHome/LandingScreen.vue";
+import ProductsOverview from "@/components/onHome/productsLine.vue";
 
 export default {
   components: {
     LandingScreen,
+    ProductsOverview,
   },
 };
 </script>
