7b8e140fa7cbfff2239c6da3a8c1d241e9ea9292
AlfClark <alfredoclarkcardenas@gmail.com>
2022-07-07 12:33:10 -0700

    Path signup and checkout views

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductView from '../views/ProductView.vue'
 import LogIn from '../views/LogIn.vue'
+import CheckOut from '../views/CheckOut.vue'
+import JoinUs from '../views/SignUp.vue'
+
 
 const routes = [
   {
@@ -18,6 +21,16 @@ const routes = [
     path: '/login',
     name: 'login',
     component: LogIn
+  },
+  {
+    path: '/joinus',
+    name: 'joinus',
+    component: JoinUs
+  },
+  {
+    path: '/checkout',
+    name: 'checkout',
+    component: CheckOut
   }
 
 ]
