c5c9c3ace7a8902c61aa3b3318d6d3982f36e8b9
AlfClark <alfredoclarkcardenas@gmail.com>
2022-07-06 17:02:48 -0700

    Path login view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductView from '../views/ProductView.vue'
+import LogIn from '../views/LogIn.vue'
 
 const routes = [
   {
@@ -12,7 +13,13 @@ const routes = [
     path: '/about',
     name: 'about',
     component: ProductView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LogIn
   }
+
 ]
 
 const router = createRouter({
