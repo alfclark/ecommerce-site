c8021817e640527868e2bf99fa3389eac401088e
AlfClark <alfredoclarkcardenas@gmail.com>
2022-07-13 15:48:08 -0700

    Component workbench start design

---
+++ src/views/AboutUs.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="aboutUs">
+    <h1 class="title">Us</h1>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.title {
+  color: var(--carbon);
+}
+</style>
